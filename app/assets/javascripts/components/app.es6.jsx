636b61aa04e44e9a8147f312410f2f97921a103c
abb1991 <alexbills1991@gmail.com>
2016-10-26 18:04:37 -0500

    moved google maps api to react componenet

--- app/assets/javascripts/components/app.es6.jsx
+++ app/assets/javascripts/components/app.es6.jsx
@@ -1,7 +1,16 @@
 class App extends React.Component {
+
   render () {
     return (
-      <h1>Hello World!</h1>
+
+      <section className="google-maps">
+        <div id="locationField">
+          <input className = "controls" id="autocomplete" placeholder="Enter your address" type="text"></input>
+          <input type="submit" value="Submit"></input>
+        </div>
+
+        <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC4lGX8l161b6OS_aa6E-x3Z_VNuvYfHkY&libraries=places&callback=initAutocomplete"> </script>
+      </section>
       )
   }
 }
